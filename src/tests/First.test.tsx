37d5af096f6b2fc68c238b7d65985a6eb237d038
Banjos <brunosilveiradosanjos@gmail.com>
2025-05-20 22:47:18 -0300

    test: finding elements

--- src/tests/First.test.tsx
+++ src/tests/First.test.tsx
@@ -1,7 +1,7 @@
-import { First } from '../First'
+import { First } from '../components/First'
 import { render, screen } from '@testing-library/react'
 
-describe('First', () => {
+describe('First test suite', () => {
   it('should render First component', () => {
     render(<First />)
     const heading = screen.getByRole('heading')
