ee5042063685d15600da5f5c482bbeb2df1c617a
Banjos <brunosilveiradosanjos@gmail.com>
2025-05-20 22:22:16 -0300

    test: first test

---
+++ src/First.tsx
@@ -0,0 +1,7 @@
+export function First() {
+  return (
+    <>
+      <h1>First</h1>
+    </>
+  )
+}
---
+++ src/tests/First.test.tsx
@@ -0,0 +1,10 @@
+import { First } from '../First'
+import { render, screen } from '@testing-library/react'
+
+describe('First', () => {
+  it('should render First component', () => {
+    render(<First />)
+    const heading = screen.getByRole('heading')
+    expect(heading).toBeInTheDocument()
+  })
+})
