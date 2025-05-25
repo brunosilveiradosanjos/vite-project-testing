bb0e5b70545c9f82892e59baf5dc4e0af6239b54
Banjos <brunosilveiradosanjos@gmail.com>
2025-05-25 17:38:19 -0300

    test: testing nav links

--- src/tests/router/RoutesConfig.test.tsx
+++ src/tests/router/RoutesConfig.test.tsx
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react'
 import { createMemoryRouter, RouterProvider } from 'react-router'
+import userEvent from '@testing-library/user-event'
 import { routesConfig } from '@/components/router/RoutesConfig'
+import { AppWithRoutes } from '@/components/router/AppWithRoutes'
 
 vi.mock('@/components/router/routes/Home', () => ({
   Home: () => <div data-testid="HomeMock" />,
@@ -16,6 +18,10 @@ vi.mock('@/components/router/routes/PageNotFound', () => ({
 
 vi.mock('@/components/router/routes/Post', () => ({
   Post: () => <div data-testid="PostMock" />,
+}))
+
+vi.mock('@/components/router/routes/Posts', () => ({
+  Posts: () => <div data-testid="PostsMock" />,
 }))
 
 describe('Routes config test suite', () => {
@@ -59,5 +65,38 @@ describe('Routes config test suite', () => {
 
     const post = screen.getByTestId('PostMock')
     expect(post).toBeInTheDocument()
+  })
+})
+
+describe('Navbar navigation tests', () => {
+  it('should show home component on home click', async () => {
+    render(<AppWithRoutes />)
+    const user = userEvent.setup()
+    const homeButton = screen.getByText('Home')
+
+    await user.click(homeButton)
+
+    const home = screen.getByTestId('HomeMock')
+    expect(home).toBeInTheDocument()
+  })
+  it('should show about component on about click', async () => {
+    render(<AppWithRoutes />)
+    const user = userEvent.setup()
+    const aboutButton = screen.getByText('About')
+
+    await user.click(aboutButton)
+
+    const about = screen.getByTestId('AboutMock')
+    expect(about).toBeInTheDocument()
+  })
+  it('should show posts component on posts click', async () => {
+    render(<AppWithRoutes />)
+    const user = userEvent.setup()
+    const postsButton = screen.getByText('Posts')
+
+    await user.click(postsButton)
+
+    const posts = screen.getByTestId('PostsMock')
+    expect(posts).toBeInTheDocument()
   })
 })
